69f478800f33e1037f5f692cee74a5e33ce1724d
sonsu <son@treehouse.finance>
2023-02-04 23:55:27 +0700

    fix redis connect server

--- cache/redis/config.go
+++ cache/redis/config.go
@@ -13,20 +13,22 @@ func GetConfig(vault *vault.Vault,config_path string) map[string]string{
 	if err!=nil{
 		err := godotenv.Load(os.ExpandEnv(".env"))
 		if err!=nil{
-			config_path=strings.TrimSuffix(config_path,"/")
-			m["HOST"]=vault.ReadVAR(fmt.Sprintf("%s/HOST",config_path))
-			m["DB"]=vault.ReadVAR(fmt.Sprintf("%s/DB",config_path))
-			m["PASSWORD"]=vault.ReadVAR(fmt.Sprintf("%s/PASSWORD",config_path))
-			m["TYPE"]=vault.ReadVAR(fmt.Sprintf("%s/TYPE",config_path))
-			m["MASTER_NAME"]=vault.ReadVAR(fmt.Sprintf("%s/MASTER_NAME",config_path))
-			return m
+			panic("Not found ENV")
 		}
+		//
+		m["HOST"]=os.Getenv("REDIS_HOST")
+		m["DB"]=os.Getenv("REDIS_DB")
+		m["PASSWORD"]=os.Getenv("REDIS_PASSWORD")
+		m["TYPE"]=os.Getenv("REDIS_TYPE")
+		m["MASTER_NAME"]=os.Getenv("REDIS_MASTER_NAME")
+		return m
 	}
-	//
-	m["HOST"]=os.Getenv("REDIS_HOST")
-	m["DB"]=os.Getenv("REDIS_DB")
-	m["PASSWORD"]=os.Getenv("REDIS_PASSWORD")
-	m["TYPE"]=os.Getenv("REDIS_TYPE")
-	m["MASTER_NAME"]=os.Getenv("REDIS_MASTER_NAME")
+	config_path=strings.TrimSuffix(config_path,"/")
+	m["HOST"]=vault.ReadVAR(fmt.Sprintf("%s/HOST",config_path))
+	m["DB"]=vault.ReadVAR(fmt.Sprintf("%s/DB",config_path))
+	m["PASSWORD"]=vault.ReadVAR(fmt.Sprintf("%s/PASSWORD",config_path))
+	m["TYPE"]=vault.ReadVAR(fmt.Sprintf("%s/TYPE",config_path))
+	m["MASTER_NAME"]=vault.ReadVAR(fmt.Sprintf("%s/MASTER_NAME",config_path))
 	return m
+	
 }
