c0e25958a97c58798dabfe57cb4efe2b8aecbe6a
KietChung <khanhcong460@gmail.com>
2023-02-27 08:34:33 +0700

    fix bug sdk

--- cache/redis/redis.go
+++ cache/redis/redis.go
@@ -168,7 +168,7 @@ func (h *RedisHelper) IncreaseMinValue(keys []string, value int) (string, *e.Err
 
 		_, err = tx.Pipelined(ctx, func(pipe redis.Pipeliner) error {
 			res = minValue + value
-			pipe.Set(ctx, key, res, time.Duration(300)*time.Second)
+			pipe.Set(ctx, key, res, time.Duration(24*365*5)*time.Hour)
 			return nil
 		})
 		return err
